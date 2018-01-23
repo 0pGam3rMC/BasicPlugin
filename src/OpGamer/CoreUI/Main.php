0e8b2c05be063a4452dea3c34359752c3536dac9
0pGam3rMC <kuasir321@gmail.com>
2018-01-22 22:44:40 -0500

    Update Main.php

--- src/OpGamer/CoreUI/Main.php
+++ src/OpGamer/CoreUI/Main.php
@@ -1,17 +1,21 @@
 <?php
-//Property of DevrlyCode & YungFlowz, you do not have permission to copy this plugin.
-//Property of DevrlyCode & YungFlowz
-//Any And All Usages Involving Non-Authorized Users Will Be Refered To As Meanie Heads
-//Copyright © @JazzyDevZ LLC
 namespace OpGamer\CoreUI;
 use pocketmine\Server;
+
 use pocketmine\Player;
+
 use pocketmine\plugin\PluginBase as Base;
+
 use pocketmine\utils\TextFormat as TF;
+
 use pocketmine\event\Listener;
+
 use pocketmine\command\Command;
+
 use pocketmine\command\CommandSender;
+
 use jojoe77777\FormAPI;
+
 class Main extends Base implements Listener{
   
     public function onEnable(){
@@ -47,19 +51,25 @@ class Main extends Base implements Listener{
                     return;
             }
         });
-        $form->setTitle(TF::BOLD . TF::GREEN . "CoreUI Menu");
-        $form->setContent(TF::GREEN . "SkyRealmPE Main Menu");
-        $form->addButton(TF::GREEN . "Discord");
-        $form->addButton(TF::GREEN . "About");
-        $form->addButton(TF::WHITE . "Back");
+        $form->setTitle(TF::BOLD . TF::GOLD . "SkyRealmPE Menu");
+        $form->setContent(TF::WHITE . "Welcome to this server's Main Menu UI Made by Crafter20162017!\n Hopefully you'll learn a thing or two while your here :) ");
+        $form->addButton(TF::GREEN . "Our Discord");
+        $form->addButton(TF::AQUA . "Our Vote website");
+        $form->addButton(TF::RED . "Back");
 	    if ($sender instanceof Player) {
         $form->sendToPlayer($sender);
 		    }
     }
     public function discordForm($sender){
-        $form->setContent(TF::WHITE . "You can join our discord at bit.do/skydiscord");	    
+        $form->setContent(TF::LIGHT_PURPLE . "You can join our discord at bit.do/skydiscord");
+	    if ($sender instanceof Player){
+		    $form->sendToPlayer($sender);
+	    }
       }  
     public function aboutForm($sender){
         $form->setContent(TF::RED . "This server was made by Crafter20162017! \n We are a BETA server meaning in development");
+	    if ($sender instanceof Player){
+		    $form->sendToPlayer($sender);
+	    }
       }
     }
